7ccf17c5de19e75ec82b0c94e7ba05d33aab7e8f
Zacck Osiemo <coderv63@gmail.com>
2023-05-21 19:25:50 +0200

    Implement walking down in the collections to get items

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn flatten<I>(iter: I) -> Flatten<I> {
+pub fn flatten<I>(iter: I) -> Flatten<I>
+where
+    I: Iterator,
+    I::Item: IntoIterator,
+{
     Flatten::new(iter)
 }
 
-pub struct Flatten<O> {
+pub struct Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
     outer: O,
+    //Cast inner item into an iterator
+    inner: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
-impl<O> Flatten<O> {
+impl<O> Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
     fn new(iter: O) -> Self {
-        Flatten { outer: iter }
+        Flatten {
+            outer: iter,
+            inner: None,
+        }
     }
 }
 
@@ -21,7 +38,19 @@ where
 {
     type Item = <O::Item as IntoIterator>::Item;
     fn next(&mut self) -> Option<Self::Item> {
-        self.outer.next().and_then(|inner| inner.into_iter().next())
+        // if there is an inner iterator and it is yeilding
+        loop {
+            if let Some(ref mut inner_iter) = self.inner {
+                if let Some(i) = inner_iter.next() {
+                    return Some(i);
+                }
+                self.inner = None;
+            }
+
+            //Pick the next item from the outer things if there is one
+            let next_inner_iter = self.outer.next()?.into_iter();
+            self.inner = Some(next_inner_iter);
+        }
     }
 }
 
@@ -43,5 +72,12 @@ mod tests {
     #[test]
     fn two_vec() {
         assert_eq!(flatten(vec![vec!["a"], vec!["b"]].into_iter()).count(), 2)
+    }
+    #[test]
+    fn empty_two_vec() {
+        assert_eq!(
+            flatten(vec![Vec::<()>::new(), vec![]].into_iter()).count(),
+            0
+        )
     }
 }
